76604b01d76c4fec17d86ef776175482181d427e
konOkoz <new2pro.ware@gmail.com>
2023-08-29 18:46:21 +0200

    implements G4-53 GET api/users/confectioner/orders.

--- src/main/java/de/ait/artcake/controller/UsersController.java
+++ src/main/java/de/ait/artcake/controller/UsersController.java
@@ -1,6 +1,8 @@
 package de.ait.artcake.controller;
 
 import de.ait.artcake.controller.api.UsersApi;
+import de.ait.artcake.dto.OrdersDto;
+import de.ait.artcake.dto.OrdersRequest;
 import de.ait.artcake.dto.UserDto;
 import de.ait.artcake.dto.UsersDto;
 import de.ait.artcake.security.details.AuthenticatedUser;
@@ -31,5 +33,10 @@ public class UsersController implements UsersApi {
         return ResponseEntity
                 .ok()
                 .body(usersService.getAllUsersByRole(role));
+    }
+    @Override
+    public ResponseEntity<OrdersDto> getAllOrdersForConfectioner(Integer pageNumber, String orderByField, Boolean desc ) {
+        return ResponseEntity
+                .ok(usersService.getAllOrdersForConfectioner(pageNumber, orderByField, desc));
     }
 }
